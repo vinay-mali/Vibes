538568d21a716a50b0af56a77138fa68e6f2522e
Vinay Mali <vinaymaliv10@gmail.com>
2026-04-09 16:47:37 +0530

    chore: added some Firebase packages and Google Fonts package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vibes/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      
+      home: SplashScreen(),
     );
   }
 }
-
