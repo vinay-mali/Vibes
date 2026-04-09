4feb01caad4f5ce01149d37710c474c39e840776
Vinay Mali <vinaymaliv10@gmail.com>
2026-04-09 17:08:39 +0530

    chore: connected Firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:vibes/firebase_options.dart';
 import 'package:vibes/screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
