a818910ec78467017f19879ffc9ef8592058b882
Vinay Mali <vinaymaliv10@gmail.com>
2026-04-10 20:19:26 +0530

    feat: Login and Sign up screen UI done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:vibes/firebase_options.dart';
-import 'package:vibes/screens/home_screen.dart';
+
 import 'package:vibes/screens/login_register_screen.dart';
-import 'package:vibes/screens/profile_set_screen.dart';
-import 'package:vibes/screens/splash_screen.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,34 +23,47 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+          titleTextStyle: GoogleFonts.poppins(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        useMaterial3: true,
       ),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return SplashScreen();
-          }
-          if (!snapshot.hasData) {
-            return LoginRegisterScreen();
-          } else {
-            return FutureBuilder(
-              future: FirebaseFirestore.instance
-                  .collection('users')
-                  .doc(snapshot.data!.uid)
-                  .get(),
-              builder: (context, userSnapshot) {
-                if (userSnapshot.connectionState == ConnectionState.waiting) {
-                  return SplashScreen();
-                }
-                if (userSnapshot.hasData && userSnapshot.data!.exists) {
-                  return HomeScreen();
-                }
-                return ProfileSetScreen();
-              },
-            );
-          }
-        },
-      ),
+      home: LoginRegisterScreen(mode: 'login'),
+      // StreamBuilder(
+      //   stream: FirebaseAuth.instance.authStateChanges(),
+      //   builder: (context, snapshot) {
+      //     if (snapshot.connectionState == ConnectionState.waiting) {
+      //       return SplashScreen();
+      //     }
+      //     if (!snapshot.hasData) {
+      //       return LoginRegisterScreen();
+      //     } else {
+      //       return FutureBuilder(
+      //         future: FirebaseFirestore.instance
+      //             .collection('users')
+      //             .doc(snapshot.data!.uid)
+      //             .get(),
+      //         builder: (context, userSnapshot) {
+      //           if (userSnapshot.connectionState == ConnectionState.waiting) {
+      //             return SplashScreen();
+      //           }
+      //           if (userSnapshot.hasData && userSnapshot.data!.exists) {
+      //             return HomeScreen();
+      //           }
+      //           return ProfileSetScreen();
+      //         },
+      //       );
+      //     }
+      //   },
+      // ),
     );
   }
 }
--- lib/screens/login_register_screen.dart
+++ lib/screens/login_register_screen.dart
@@ -1,15 +1,187 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:vibes/widgets/app_text.dart';
 
 class LoginRegisterScreen extends StatefulWidget {
-  const LoginRegisterScreen({super.key});
+  final String mode;
+  const LoginRegisterScreen({super.key, required this.mode});
 
   @override
   State<LoginRegisterScreen> createState() => _LoginRegisterScreenState();
 }
 
 class _LoginRegisterScreenState extends State<LoginRegisterScreen> {
+  TextEditingController emailCtrl = TextEditingController();
+  TextEditingController passwordCtrl = TextEditingController();
+  bool showPassword = true;
+  bool _isLoading = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text(widget.mode == 'login' ? "Log in" : "Sign up"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFFAA88E8), Color(0xFF88C8F0)],
+            ),
+          ),
+          child: SafeArea(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.of(context).size.height,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      bottom: 10,
+                      left: 15,
+                      right: 15,
+                    ),
+                    child: TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      cursorColor: const Color.fromARGB(255, 77, 40, 142),
+                      style: GoogleFonts.poppins(color: Colors.black),
+                      controller: emailCtrl,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.email),
+                        labelText: "Email",
+                        labelStyle: GoogleFonts.poppins(color: Colors.black),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: Colors.black, width: 1),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: Colors.black, width: 2),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      bottom: 10,
+                      left: 15,
+                      right: 15,
+                    ),
+                    child: TextField(
+                      cursorColor: Colors.black,
+                      obscureText: showPassword,
+                      style: GoogleFonts.poppins(),
+                      controller: passwordCtrl,
+                      decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              showPassword = !showPassword;
+                            });
+                          },
+                          icon: Icon(
+                            showPassword
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                        ),
+                        prefixIcon: Icon(Icons.password),
+                        labelText: "Password",
+                        labelStyle: GoogleFonts.poppins(color: Colors.black),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: Colors.black, width: 1),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: Colors.black, width: 2),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 15,
+                      left: 15,
+                      right: 15,
+                    ),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 55,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            35,
+                            87,
+                            178,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        child: !_isLoading
+                            ? AppText(
+                                text: widget.mode == 'login'
+                                    ? "Log in"
+                                    : "Sign up",
+                                textColor: Colors.white,
+                                textFontSize: 18,
+                              )
+                            : SizedBox(
+                                height: 20,
+                                width: 20,
+                                child: Center(
+                                  child: CircularProgressIndicator(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      if (widget.mode == 'login') {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                LoginRegisterScreen(mode: 'signup'),
+                          ),
+                        );
+                      } else {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                LoginRegisterScreen(mode: 'login'),
+                          ),
+                        );
+                      }
+                    },
+                    child: AppText(
+                      text: widget.mode == 'login'
+                          ? "Don't have an account? Create now."
+                          : "Already have an account? Log in.",
+                      textColor: const Color.fromARGB(255, 54, 25, 103),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
